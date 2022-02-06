dbda4bb59913b609e63db7c47c0b4cb0f0fdda84
PR <przemorog01@gmail.com>
2022-02-06 19:14:22 +0100

    trying to make it work

--- script.js
+++ script.js
@@ -1,40 +1,197 @@
 (function () {
 
+   const tiles = [
+
+      [
+         [0, 1, 0, 0],
+         [0, 1, 1, 0],
+         [0, 0, 1, 0],
+         [0, 0, 0, 0]
+      ],
+      [
+         [0, 1, 1, 0],
+         [0, 1, 1, 0],
+         [0, 0, 0, 0],
+         [0, 0, 0, 0]
+      ],
+      [
+         [0, 1, 1, 0],
+         [0, 0, 1, 0],
+         [0, 0, 1, 0],
+         [0, 0, 0, 0]
+      ],
+
+      [
+         [0, 0, 1, 0],
+         [0, 0, 1, 0],
+         [0, 0, 1, 0],
+         [0, 0, 1, 0]
+      ]
+
+   ];
+   let tileArr = [];
+   const colors = [
+      {x : "#f5ef42", y : "#000000"}, 
+      {x : "#c70e1d", y : "#000000"}, 
+      {x : "#4287f5", y : "#000000"},
+      {x : "#00FFFE", y : "#000000"}, 
+      {x : "#f54298", y : "#000000"}
+   ];
+
+   let board = [];
+   
    let canvas = document.getElementById('canvas');
    let ctx = canvas.getContext("2d");
-   const WALL_COLOR = "#000000";
-   const TILE_SIZE = 20;
-   const ROWS = canvas.height / TILE_SIZE;
-   const COLUMNS = canvas.width / TILE_SIZE;
+   let speed = 100;
+   const BRICK_SIZE = 25;
+   let randomTile = utils.randomNumber(0, tiles.length - 1);
+   console.log(randomTile)
+
+
 
    class Tile {
-      constructor(x, y, color) {
+      constructor(x, y, bgColor, bdColor) {
          this.x = x;
          this.y = y;
-         ctx.fillStyle = color;
-         ctx.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
-         ctx.fillStyle = '#4287f5';
-         ctx.fillRect(x * TILE_SIZE + 1, y * TILE_SIZE + 1, TILE_SIZE - 2, TILE_SIZE -2);
+         this.bgColor = bgColor;
+         this.bdColor = bdColor;
       }
+
+      draw() {
+         ctx.fillStyle = this.bdColor;
+         ctx.fillRect(this.x * BRICK_SIZE, this.y * BRICK_SIZE, BRICK_SIZE, BRICK_SIZE);
+         ctx.fillStyle = this.bgColor;
+         ctx.fillRect(this.x * BRICK_SIZE + 1, this.y * BRICK_SIZE + 1, BRICK_SIZE - 1, BRICK_SIZE - 1);
+      }
+
+      clear() {
+         ctx.clearRect(this.x * BRICK_SIZE, this.y * BRICK_SIZE, BRICK_SIZE, BRICK_SIZE);
+      }
+   }
+
+
+   class Shape {
+      constructor() {
+         this.createRandomShape();
+      }
+
+      createRandomShape() {
+         tileArr = [];
+         randomTile = utils.randomNumber(0, tiles.length - 1);
+         // let randomTile = utils.randomNumber(0, tiles.length - 1);
+         let randomColor = Math.floor(Math.random() * colors.length + 0);
+         let tile = tiles[randomTile];
+
+
+         for (let i = 0; i < 4; i++) {
+            for (let j = 0; j < 4; j++) {
+               if (tile[i][j]) {
+                  let tilex = new Tile(8 + j, i, colors[randomColor].x, colors[randomColor].y);
+                  tileArr.push(tilex);
+                  tilex.draw();
+               }
+            }
+         }
+      }
+   }
+
+
+   class Game {
+      constructor() {
+         this.play();
+      }
+
+      play() {
+         requestAnimationFrame(move);
+      }
+   }
+
+   function move() {
+      setTimeout(function () {
+         if (boardEndY()) {
+            tileArr.forEach(elem=>{
+               board.push(elem);
+            })
+            console.log(board)
+            new Shape();
+         }
+
+         tileArr.forEach(element => {
+            element.clear();
+         });
+
+         tileArr.forEach(element => {
+            element.y += 1;
+            element.draw();
+         });
+         requestAnimationFrame(move);
+      }, speed);
+
+
 
    }
-   
-   class Game{
-      constructor(){
-         this.createWalls();
-      }
 
-       createWalls(){
-         for (let i = 0; i < ROWS; i++) {
-            new Tile(0, i, WALL_COLOR);
-            new Tile(COLUMNS - 1, i, WALL_COLOR);
+   document.body.onkeyup = function (e) {
+      if (e.keyCode == 40) {
+         if (boardEndY()) {
+            return;
          }
-         for (let i = 0; i < COLUMNS; i++) {
-            new Tile(i, 0, WALL_COLOR);
-            new Tile(i, ROWS - 1, WALL_COLOR);
-         }
-      }
+         tileArr.forEach(element => {
+            element.clear();
+         });
+         tileArr.forEach(element => {
+            element.y += 1;
+            element.draw();
+         });
 
+      } else if (e.keyCode == 37) {
+         if (shouldStopMovingX(0)) {
+            return;
+         }
+         tileArr.forEach(element => {
+            element.clear();
+         });
+         tileArr.forEach(element => {
+            element.x -= 1;
+            element.draw();
+         });
+
+      } else if (e.keyCode == 39) {
+         if (shouldStopMovingX(19)) {
+            return;
+         }
+         tileArr.forEach(element => {
+            element.clear();
+         });
+         tileArr.forEach(element => {
+            element.x += 1;
+            element.draw();
+         });
+      }
+   }
+
+   function shouldStopMovingX(posX) {
+      let exit = false;
+      tileArr.forEach(element => {
+         if (element.x == posX) {
+            exit = true;
+         }
+      })
+      return exit;
+   }
+
+   function boardEndY() {
+      let exit = false;
+      if (!tileArr.length) {
+         return true;
+      } else {
+         tileArr.forEach(element => {
+            if (element.y == 23) {
+               exit = true;
+            }
+         })
+         return exit;
+      }
    }
 
    new Game();
---
+++ utils.js
@@ -0,0 +1,11 @@
+let utils = (function(){
+    function getRandomNumber(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    return {
+        randomNumber : getRandomNumber,
+    }
+
+    
+}())
